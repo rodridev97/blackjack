36d3a4ab95ed9cdac7f3c4441e1b5354886e4e55
rodri.dev97 <rodri.dev97@gmail.com>
2023-07-17 14:58:17 -0700

    Complete Game

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,103 @@
 "use strict";
 let deck = [];
+const typosCard = ['C', 'D', 'H', 'S'];
+const cardLetterSpecial = ['J', 'Q', 'K', 'A'];
+let playerPoints = 0, cpuPoints = 0;
+const btnPick = document.querySelector('#btnPick');
+const btnStop = document.querySelector('#btnStop');
+const btnNew = document.querySelector('#btnNew');
+const pointsHTML = document.querySelectorAll('small');
+const divPlayerCard = document.querySelector('#player-card');
+const divPlayerCardCPU = document.querySelector('#cpu-card');
 const createDeck = () => {
     for (let i = 2; i <= 10; i++) {
-        deck.push(`${i}C`);
+        for (let t in typosCard) {
+            deck.push(`${i + typosCard[t]}`);
+        }
     }
-    console.log(deck);
+    for (let tipo of typosCard) {
+        for (let esp of cardLetterSpecial) {
+            deck.push(esp + tipo);
+        }
+    }
+    deck = _.shuffle(deck);
+    return deck;
 };
 createDeck();
+const pickCard = () => {
+    if (deck.length === 0) {
+        throw 'The deck is Empty';
+    }
+    const card = deck.pop();
+    return card;
+};
+const valueCard = (card) => {
+    const value = card.substring(0, card.length - 1);
+    return (isNaN(value)) ? ((value === 'A') ? 11 : 10) : value * 1;
+};
+const cardSelected = pickCard();
+const value = valueCard(cardSelected);
+const turnComputer = (pointsMin) => {
+    do {
+        const cardPick = pickCard();
+        cpuPoints = cpuPoints + valueCard(cardPick);
+        pointsHTML[1].innerHTML = cpuPoints.toString();
+        const imgCard = document.createElement('img');
+        imgCard.className = 'card';
+        imgCard.src = `../assets/cartas/${cardPick}.png`;
+        divPlayerCardCPU === null || divPlayerCardCPU === void 0 ? void 0 : divPlayerCardCPU.append(imgCard);
+        if (pointsMin > 21) {
+            break;
+        }
+    } while ((cpuPoints < playerPoints) && (pointsMin <= 21));
+    if (cpuPoints === pointsMin) {
+        alert('Pass !!!');
+    }
+    else if (pointsMin > 21) {
+        alert('Computer Winner :0 ');
+    }
+    else if (cpuPoints > 21) {
+        alert(' Player Winner !!! :) ');
+    }
+    else {
+        alert('Computer winner');
+    }
+};
+btnPick === null || btnPick === void 0 ? void 0 : btnPick.addEventListener('click', () => {
+    const cardPick = pickCard();
+    playerPoints = playerPoints + valueCard(cardPick);
+    pointsHTML[0].innerHTML = playerPoints.toString();
+    const imgCard = document.createElement('img');
+    imgCard.className = 'card';
+    imgCard.src = `../assets/cartas/${cardPick}.png`;
+    divPlayerCard === null || divPlayerCard === void 0 ? void 0 : divPlayerCard.append(imgCard);
+    if (playerPoints > 21) {
+        console.error(`I'am sorry, Game Over !!!`);
+        btnPick.setAttribute('disabled', 'true');
+        btnStop === null || btnStop === void 0 ? void 0 : btnStop.setAttribute('disabled', 'true');
+        turnComputer(playerPoints);
+    }
+    else if (playerPoints === 21) {
+        alert('Congrats, you are winner!!');
+        btnPick.setAttribute('disabled', 'true');
+        btnStop === null || btnStop === void 0 ? void 0 : btnStop.setAttribute('disabled', 'true');
+        turnComputer(playerPoints);
+    }
+});
+btnStop === null || btnStop === void 0 ? void 0 : btnStop.addEventListener('click', () => {
+    btnPick === null || btnPick === void 0 ? void 0 : btnPick.setAttribute('disabled', 'true');
+    btnStop === null || btnStop === void 0 ? void 0 : btnStop.setAttribute('disabled', 'true');
+    turnComputer(playerPoints);
+});
+btnNew === null || btnNew === void 0 ? void 0 : btnNew.addEventListener('click', () => {
+    deck = [];
+    deck = createDeck();
+    btnPick === null || btnPick === void 0 ? void 0 : btnPick.removeAttribute('disabled');
+    btnStop === null || btnStop === void 0 ? void 0 : btnStop.removeAttribute('disabled');
+    playerPoints = 0;
+    cpuPoints = 0;
+    pointsHTML[0].innerText = '0';
+    pointsHTML[1].innerText = '0';
+    divPlayerCard.innerHTML = "";
+    divPlayerCardCPU.innerHTML = "";
+});
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,16 +1,146 @@
+/// <reference path="underscore.browser.d.ts" />
 
-
+// BLACKJACK APP //
 let deck:string[] = [];
 
-enum TyposCard { 'C', 'D', 'H', 'S' }
+const typosCard:string[]         = [ 'C', 'D', 'H', 'S' ];
+const cardLetterSpecial:string[] = [ 'J', 'Q', 'K', 'A'];
 
+let playerPoints = 0,
+    cpuPoints = 0;
+
+// Reference to HTML
+const btnPick = document.querySelector('#btnPick'); 
+const btnStop = document.querySelector('#btnStop');  
+const btnNew  = document.querySelector('#btnNew');
+
+const pointsHTML = document.querySelectorAll('small');
+
+const divPlayerCard = document.querySelector('#player-card');
+const divPlayerCardCPU = document.querySelector('#cpu-card');
+
+// This function create a new deck
 const createDeck = () => {
     for (let i:number = 2; i <= 10; i++) {
-        deck.push( `${i}` )
+        for (let t in typosCard) {
+            deck.push( `${i + typosCard[t]}` );
+        }
     }
 
-    console.log(deck)
+    for (let tipo of typosCard) {
+        for (let esp of cardLetterSpecial) {
+            deck.push( esp + tipo );
+        }   
+    }
+
+    deck = _.shuffle(deck);
+    
+    return deck;
+}
+createDeck();
+// This function pick a card 
+
+const pickCard = () => {
+    
+    if ( deck.length === 0 ) {
+        throw 'The deck is Empty';
+    }
+
+    const card:string = deck.pop()!;
+    
+    return card;
 }
 
-createDeck()
+const valueCard = ( card:string ) => {
 
+    const value:any  = card.substring(0, card.length -1);
+    return ( isNaN(value) ) ? ( ( value === 'A' ) ? 11 : 10 ) : value * 1;
+}
+
+const cardSelected = pickCard()  
+const value = valueCard( cardSelected );
+
+// Turn of Computer
+const turnComputer = ( pointsMin:number ) => {
+
+    do {
+        const cardPick = pickCard();
+        cpuPoints = cpuPoints + valueCard( cardPick );
+        pointsHTML[1].innerHTML = cpuPoints.toString();
+
+        // Create image
+        const imgCard = document.createElement('img');
+        imgCard.className = 'card';
+        imgCard.src = `../assets/cartas/${cardPick}.png`;
+        divPlayerCardCPU?.append(imgCard);    
+
+        if ( pointsMin > 21 ) {
+            break;
+        }
+
+    } while ( (cpuPoints < playerPoints) && ( pointsMin <= 21 ) );
+    
+
+    if ( cpuPoints === pointsMin ) {
+        alert('Pass !!!');
+    } else if ( pointsMin > 21 ) {
+        alert('Computer Winner :0 ')
+    } else if ( cpuPoints > 21 ) {
+        alert(' Player Winner !!! :) ')
+    } else {
+        alert('Computer winner')
+    }
+
+}
+
+
+// Events
+btnPick?.addEventListener('click', () => {
+
+    const cardPick = pickCard();
+    playerPoints = playerPoints + valueCard( cardPick );
+    pointsHTML[0].innerHTML = playerPoints.toString();
+
+    // Create image
+    const imgCard = document.createElement('img');
+    imgCard.className = 'card';
+    imgCard.src = `../assets/cartas/${cardPick}.png`;
+    divPlayerCard?.append(imgCard);
+
+    if ( playerPoints > 21 ) {
+        console.error( `I'am sorry, Game Over !!!` );
+        btnPick.setAttribute('disabled','true');
+        btnStop?.setAttribute('disabled','true');
+        turnComputer( playerPoints );
+    } else if ( playerPoints === 21 ) {
+        alert('Congrats, you are winner!!');
+        btnPick.setAttribute('disabled','true');
+        btnStop?.setAttribute('disabled','true');
+        turnComputer( playerPoints );
+    }
+
+});
+
+btnStop?.addEventListener('click', () => {
+    btnPick?.setAttribute('disabled','true');
+    btnStop?.setAttribute('disabled','true');
+    turnComputer( playerPoints );  
+});
+
+
+btnNew?.addEventListener('click', () => {
+
+    deck = [];
+    deck = createDeck();
+    btnPick?.removeAttribute('disabled');
+    btnStop?.removeAttribute('disabled');
+    playerPoints = 0;
+    cpuPoints = 0;
+
+    pointsHTML[0].innerText = '0';
+    pointsHTML[1].innerText = '0';
+
+    divPlayerCard!.innerHTML = "";
+    divPlayerCardCPU!.innerHTML = "";
+
+});
